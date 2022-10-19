11dda220c7a60a02656a4a269658f6c90a834255
clivetran <tranvinhnhan.tech.clone@gmail.com>
2022-10-19 16:45:29 +0700

    Add insertion sort

--- Algorithms/InsertionSort.cs
+++ Algorithms/InsertionSort.cs
@@ -2,13 +2,13 @@
 {
     public static class InsertionSort
     {
-        public static void Sort(int[] arr, int n)
+        public static void Sort(int[] arr)
         {
-            for (int i = 1; i < n; i++)
+            for (int i = 1; i < arr.Length; ++i)
             {
                 int key = arr[i];
                 int j = i - 1;
-                while (j > 0 && arr[j] > key)
+                while (j >= 0 && arr[j] > key)
                 {
                     arr[j + 1] = arr[j];
                     j--;
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Algorithms;
+
+int[] arr = { 5, 2, 4, 6, 1, 3 };
+InsertionSort.Sort(arr);
